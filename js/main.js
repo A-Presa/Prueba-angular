87cff51a28ab08d8c2365705ca08d5f031f19b28
A-Presa <presa.aitor@gmail.com>
2017-01-17 13:56:01 +0100

    Añadida una prueba de directivas personalizadas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-var miapp = angular.module('miApp',[]);
-miapp.controller ('controlador1',['$scope',function($scope){
+var miapp = angular.module('miApp', []);
+
+miapp.controller('controlador1', ['$scope', function ($scope) {
     $scope.nombre = "Aitor"
 }]);
+
+miapp.directive('tuNombre',function(){
+    return{
+        template:'Tu nombre es {{minombre}}'
+    }
+})
