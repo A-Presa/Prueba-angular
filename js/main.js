6fdbe2513c7cc91aa631d4d6bba7f0136d39697e
A-Presa <presa.aitor@gmail.com>
2017-01-18 11:35:36 +0100

    ejemplos de datos remotos

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
 var miapp = angular.module('miApp', []);
 
-miapp.controller('controlador1', ['$scope', function ($scope) {
+miapp.controller('controlador1', function ($scope, $http) {
     $scope.nombre = "Aitor"
-}]);
 
-miapp.directive('tuNombre',function(){
-    return{
-        template:'Tu nombre es {{minombre}}'
+    $http.get('productos.json')
+        .then(function (res) {
+            $scope.listaProductos = res.data;
+        });
+var url = 'http://mysafeinfo.com/api/data?list=englishmonarchs&format=json';
+    $http.get(url)
+        .then(function (res) {            
+            console.log(res) 
+            $scope.datos = res.data 
+        });
+});
+
+miapp.directive('tuNombre', function () {
+    return {
+        template: 'Tu nombre es {{minombre}}'
     }
 })
